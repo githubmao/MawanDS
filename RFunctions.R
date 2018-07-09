eb7e9667482351775dc9121cbb55dcddb29f1eba
githubmao <maoyanbit@foxmail.com>
2018-07-09 16:18:43 +0800

    用于分析的R函数

--- RFunctions.R
+++ RFunctions.R
@@ -106,3 +106,93 @@ RenameSimDataV12 <- function(data){
   names(data) <- kSimDataNewName
   return(data)
 }
+
+
+# 计算行车轨迹----
+CalcDrivingTraj <- function(data,
+                            kMainRoadName = NA,
+                            kRampRoadName = NA,
+                            kDeltaDisBorder = NA){
+  # 依据 dsToLeftBorder 计算模拟车行车轨迹。
+  #
+  # 输入：
+  # data：重命名后的数据框，含 disToLeftBorder, roadName等数据。
+  # is.main2ramp：主线进入匝道为 TRUE，匝道进入主线为 FALSE，默认为TRUE。
+  # kMainRoadName：主线道路名称，默认为NA。
+  # kRampRoadName：匝道道路名称，默认为NA。
+  # kDeltaDisBorder：主线与匝道Traj计算差，默认为NA。
+  #
+  # 输出：含行车轨迹变量 drivingTraj 的数据框。
+
+  if (is.na(kDeltaDisBorder)) {
+    stop("Please input the 'kDeltaDisBorder'.")  # 没有输入kDeltaDisBorder
+  } else if (is.na(kMainRoadName) | is.na(kRampRoadName)) {
+    stop("Please input the 'road name'.")  # 没有输入RoadName
+  } else {
+    
+    data$DrivingTraj <- ifelse(test = data$roadName == kMainRoadName,
+                               data$disToLeftBorder,
+                               data$disToLeftBorder + kDeltaDisBorder)
+  }
+  return(data)  # 返回新数据框
+}
+
+
+# 计算CalcDrivingTraj中的 kDeltaDisBorder 参数----
+CalcDeltaDisBorder <- function(data,
+                               is.main2ramp = TRUE,
+                               is.disdecrease = FALSE){
+  # 计算CalcDrivingTraj中的 kDeltaDisBorder 参数。
+  #
+  # 输入：
+  # data：重命名后的数据框，含 disToLeftBorder, roadName等数据。
+  # is.main2ramp：主线进入匝道为 TRUE，匝道进入主线为 FALSE，默认为TRUE。
+  # is.disdecrease：data中的disTraveled变量是否为递减趋势，默认为FALSE。
+  #
+  # 输出：kDeltaDisBorder计算值。
+  
+  data <- data[order(data$disTravelled,
+                     decreasing = is.disdecrease),]  # data按桩号排序
+  
+  kRoadName <- unique(data$roadName)  # 数据所含道路名称
+  
+  tmp.subdata1 <- subset(x = data, subset = data$roadName == kRoadName[1])
+  tmp.subdata2 <- subset(x = data, subset = data$roadName == kRoadName[2])
+  
+  if (length(kRoadName) != 2) {
+    stop("The number of 'road name' is not equal to 2.")  # 异常情况
+  } else {
+    
+    kData1Length <- length(tmp.subdata1$disToLeftBorder)  # tmp.subdata1行数
+    
+    # 主线进入匝道
+    kDeltaDisBorder <- ifelse(test = is.main2ramp,
+                              tmp.subdata1[kData1Length,]$disToLeftBorder -
+                                tmp.subdata2[1,]$disToLeftBorder,
+                              tmp.subdata2[1,]$disToLeftBorder -
+                                tmp.subdata1[kData1Length,]$disToLeftBorder)
+  }
+  return(kDeltaDisBorder)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
